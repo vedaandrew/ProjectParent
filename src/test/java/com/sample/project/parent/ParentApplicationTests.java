3604ae9eea0e50aebbc0a8c6f735972a0083622d
VAArockiam <vaarockiam@Vedas-MacBook-Pro.local>
2024-05-24 17:39:34 +0100

    project parent changes

---
+++ src/test/java/com/sample/project/parent/ParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.project.parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
